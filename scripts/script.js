5d610a51ccc4e8963679dcef36607fc5298080df
tristen <tristen@tristen.ca>
2011-07-26 11:15:01 -0400

    Adding in interaction, removing formatter - no need for it

--- scripts/script.js
+++ scripts/script.js
@@ -10,10 +10,10 @@ var layers = 'usa1-census-state-z2-5,';
 
 function getTiles() {
     return [
-        "http://a.tiles.mapbox.com/devseed/1.0.0/"+layers+"/{z}/{x}/{y}.jpg",
-        "http://b.tiles.mapbox.com/devseed/1.0.0/"+layers+"/{z}/{x}/{y}.jpg",
-        "http://c.tiles.mapbox.com/devseed/1.0.0/"+layers+"/{z}/{x}/{y}.jpg",
-        "http://d.tiles.mapbox.com/devseed/1.0.0/"+layers+"/{z}/{x}/{y}.jpg"
+        "http://a.tiles.mapbox.com/devseed/1.0.0/"+layers+"/{z}/{x}/{y}.png",
+        "http://b.tiles.mapbox.com/devseed/1.0.0/"+layers+"/{z}/{x}/{y}.png",
+        "http://c.tiles.mapbox.com/devseed/1.0.0/"+layers+"/{z}/{x}/{y}.png",
+        "http://d.tiles.mapbox.com/devseed/1.0.0/"+layers+"/{z}/{x}/{y}.png"
         ];
 };
 
@@ -26,24 +26,20 @@ function getGrids() {
         ];
 };
 
-console.log(getTiles());
-
 var tilejson = {
     tilejson: '1.0.0',
     scheme: 'tms',
     tiles: getTiles(),
     grids: getGrids(),
     minzoom: 4,
-    maxzoom: 12,
-    formatter: function(options, data) {
-        console.log(data);
-    }
+    maxzoom: 12
 };
 var mm = com.modestmaps;
 var m = new mm.Map('map', new wax.mm.connector(tilejson));
 wax.mm.interaction(m, tilejson);
 m.setCenterZoom(new mm.Location(39, -98), 5);
 legend = wax.mm.legend(m, tilejson).appendTo(m.parent);
+wax.mm.interaction(m, tilejson);
 wax.mm.zoomer(m, tilejson).appendTo(m.parent);
 
 function zipcodeYDN(zip) {
@@ -54,7 +50,7 @@ function zipcodeYDN(zip) {
                 $.each(resp.postalcodes, function(value) {
                     var lat = value.lat,
                         lng = value.lng;
-                    console.log(lat , lng);
+                    m.setCenterZoom(new mm.Location(lat, lng), 12);
                 });
         }
     });
